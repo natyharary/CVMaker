e4ed034da611f2c1cefc0a4e066dbd946db154ac
Nathan Engelberg <nathanen@wismain.weizmann.ac.il>
2019-02-18 15:12:54 +0200

    Initial commit

---
+++ src/com/gmail/dotharary/naty/CVMaker/main.java
@@ -0,0 +1,9 @@
+package com.gmail.dotharary.naty.CVMaker;
+
+/* ===============================================================================
+CVMaker is a graphing app for creating Cyclic Voltammetry graphs out of txt files.
+Graphing is done with the open-license GRAL Java Graphing library.
+=============================================================================== */
+
+public class main {
+}
