733ab9fdd0432177b3f92fff2d7876cbc8cef132
nathanen <naty.harary@gmail.com>
2019-02-26 18:09:37 +0200

    26.2.19 end of day in lab

--- src/com/gmail/dotharary/naty/CVMaker/application.java
+++ src/com/gmail/dotharary/naty/CVMaker/application.java
@@ -28,7 +28,8 @@ public class application {
     public static void main(String[] args) {
         String[] inputArgs = userInput();
         ArrayList readLines = readFile(inputArgs[1], inputArgs[0]);
-        parseData(readLines, inputArgs[2]);
+        ArrayList returnedLists = parseData(readLines, inputArgs[2]);
+        graphData(returnedLists);
     }
 
     // TODO add options to add multiple files
@@ -45,19 +46,23 @@ public class application {
         while (!addmore.equals("N")) {
             // Take user input for first filename. This will loop until Done or done is entered
             System.out.println("Please input file name and then Enter:\n(example: for Nitrogen.txt, type Nitrogen) to add to graph, or Done to continue:");
-            inputArgs[0] = reader.nextLine();
+            /*inputArgs[0] = reader.nextLine();*/ //TODO UNDO THIS LATER
+            inputArgs[0] = "test.txt";
 
             // Take file path from the user, for the filename specified
             System.out.println("Please input the path for the file " + inputArgs[0] + " and then Enter\n" +
                     "(example: for file C:\\MyFolder\\data\\Nitrogen.txt, type C:\\MyFolder\\Results)");
-            inputArgs[1] = reader.nextLine();
+            /*inputArgs[1] = reader.nextLine();*/ // TODO UNDO TIS LATER
+            inputArgs[1] = "C:\\CVMaker_data";
 
             System.out.println("For the file: " + inputArgs[1] + "\\" + inputArgs[0] + ",\nPlease input cycle # to be used");
-            inputArgs[2] = (reader.nextLine());
+            /*inputArgs[2] = (reader.nextLine());*/ //TODO UNDO TIS LATER
+            inputArgs[2] = "1";
 
             // TODO add option for multiple cycle choice
             System.out.println("Do you want to add more files? (Y/N)");
-            addmore = reader.nextLine();
+            /*addmore = reader.nextLine();*/ //TODO UNDO TIS LATER
+            addmore = "N";
         }
         return inputArgs;
     }
@@ -79,20 +84,20 @@ public class application {
         return readLines;
     }
 
-    public static ArrayList<ArrayList<Integer>> parseData(ArrayList readLines, String inputtedUserCycle) {
+    public static ArrayList<ArrayList<Double>> parseData(ArrayList readLines, String inputtedUserCycle) {
         // Defining ArrayLists for data
-        ArrayList<Integer> voltageList = new ArrayList<Integer>();
-        ArrayList<Integer> currentList = new ArrayList<Integer>();
-        ArrayList<Integer> cycleList = new ArrayList<Integer>();
+        ArrayList<Double> voltageList = new ArrayList<>();
+        ArrayList<Double> currentList = new ArrayList<>();
+        ArrayList<Double> cycleList = new ArrayList<>();
         // Transforming inputtedUserCycle from String to int to userCycle
         // TODO find a smarter way to transform user input from string to integer. Maybe to make the array contain int and string?...
         int userCycle = Integer.parseInt(inputtedUserCycle);
 
-        // TODO change the counter later to filter out according to "mode ox/red" parameters line
+        // TODO change the counter later to filter out according to the "mode ox/red" parameters line
         for (int counter = 56; counter < readLines.size(); counter++) {
             /*            String currentLine = (String) readLines.get(counter); //currentLine holds the current line now read from the readLines ArrayList*/
 
-            ArrayList<String> parsedLine = new ArrayList<String>();
+            ArrayList<String> parsedLine = new ArrayList<>();
 
             String currentLine = (String) readLines.get(counter);
             for (String str : currentLine.split("\t")) {
@@ -105,16 +110,32 @@ public class application {
             // Select only the lines which contain the cycle the user chose, which is userCycle
             if (lineCycle == userCycle) {
 
+
+                /* // TODO THIS IS AN OLD PIECE OF CODE, BACK WHEN I USED int AS THE DATA TYPE OF THE DATA SERIES
+                *//* Adding Voltage to voltageList first
+                This is a bit hairy: I had to convert bigDecimal -> double -> int, because the number is in scientific notation
+                (requires bigDecimal) and converting bigDecimal to int directly gives 0 value*//*
+                BigDecimal decimalVolt = new BigDecimal(parsedLine.get(7));
+                double doubleVoltage = decimalVolt.doubleValue();
+                int intVoltage = (int) doubleVoltage;
+                voltageList.add(intVoltage);
+                */
+
                 //Assigning variables out of parsedLine to ArrayLists called voltageList, currentList and cycleList,
-                // also transforming them into integers
-                voltageList.add(Integer.parseInt(parsedLine.get(7)));
-                currentList.add(Integer.parseInt(parsedLine.get(8)));
-                cycleList.add(Integer.parseInt(parsedLine.get(9)));
+                double doubleVoltage = Double.parseDouble(parsedLine.get(7));
+                voltageList.add(doubleVoltage);
+
+                double doubleCurrent = Double.parseDouble(parsedLine.get(8));;
+                currentList.add(doubleCurrent);
+
+                // Cycle is just an integer, but it is assigned as double since I cant return arrayLists of mixed data types
+                double doubleCycle = Double.parseDouble(parsedLine.get(9));
+                cycleList.add(doubleCycle);
             }
         }
 
         // Creating an ArrayList called returnedLists, to return all 3 ArrayLists containing the parsed data
-        ArrayList<ArrayList<Integer>> returnedLists = new ArrayList<ArrayList<Integer>>(3);
+        ArrayList<ArrayList<Double>> returnedLists = new ArrayList<>(3);
         returnedLists.add(voltageList);
         returnedLists.add(currentList);
         returnedLists.add(cycleList);
@@ -122,22 +143,23 @@ public class application {
         return returnedLists;
     }
 
-
-    // TODO CONTINUE FROM HERE!!! USE THE DATA COLLECTED BY parseData TO MAKE SOME NEAT GRAPHS!
-    public static void graphData() {
+    public static void graphData(ArrayList returnedLists) {
         // Generate data
         DataTable data = new DataTable(Double.class, Double.class);
 
+        // Get numbers of arrayList
+        double num1 = (double) returnedLists.get(0);
+
         // Create data series
-        DataSeries seriesLog = new DataSeries(data, 0, 2, 3, 4);
-        DataSeries seriesLin = new DataSeries(data, 0, 1, 5);
+        DataSeries seriesVoltage = new DataSeries(data, (int) num1);
+        DataSeries seriesCurrent = new DataSeries(data, 0, 1, 2);
 
         // Create new xy-plot
-        XYPlot plot = new XYPlot(seriesLog, seriesLin);
+        XYPlot plot = new XYPlot(seriesVoltage, seriesCurrent);
 
-
+/*
         // TODO THIS IS FOR JPG EXPORT
-/*        private byte[] getJpg() throws IOException {
+        private byte[] getJpg() throws IOException {
             BufferedImage bImage = new BufferedImage(800, 600, BufferedImage.TYPE_INT_ARGB);
             Graphics2D g2d = (Graphics2D) bImage.getGraphics();
             DrawingContext context = new DrawingContext(g2d);
